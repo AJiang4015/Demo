f14e56308dedb22570c33a8767980f3affabeef5
ajiang <int4015@gamil.com>
2025-06-22 15:54:42 +0800

    文档完善

--- user-service/src/main/java/com/ajiang/userservice/util/SeataTransactionUtil.java
+++ user-service/src/main/java/com/ajiang/userservice/util/SeataTransactionUtil.java
@@ -17,32 +17,6 @@ import org.springframework.stereotype.Component;
 public class SeataTransactionUtil {
 
     /**
-     * 记录当前全局事务ID
-     */
-    public static void logCurrentXid(String operation) {
-        String xid = RootContext.getXID();
-        if (xid != null) {
-            log.info("[分布式事务] 操作: {}, 全局事务ID: {}", operation, xid);
-        } else {
-            log.info("[分布式事务] 操作: {}, 当前无全局事务", operation);
-        }
-    }
-
-    /**
-     * 检查是否在全局事务中
-     */
-    public static boolean isInGlobalTransaction() {
-        return RootContext.inGlobalTransaction();
-    }
-
-    /**
-     * 获取当前全局事务ID
-     */
-    public static String getCurrentXid() {
-        return RootContext.getXID();
-    }
-
-    /**
      * 记录事务开始
      */
     public static void logTransactionStart(String businessOperation) {
